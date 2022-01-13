7585867d66fccd8593f06d17719462641b999bfa
MATHIEU221 <95666896+MATHIEU221@users.noreply.github.com>
2022-01-13 18:05:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+a=[];
+function s(){
+
+var n1=document.getElementById("student_1").value;
+var n2=document.getElementById("student_2").value;
+var n3=document.getElementById("student_3").value;
+var n4=document.getElementById("student_4").value;
+a.push(n1);
+a.push(n2);
+a.push(n3);
+a.push(n4);
+
+document.getElementById("t").innerHTML=a
+
+}
+
+
+
+
